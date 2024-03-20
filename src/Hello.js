7c8086f6ffa9113acd1cef6d845d3c0195a3b723
Jesse Smart <jsmartdev@protonmail.com>
2024-03-20 18:29:58 -0400

    created hello component

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Hello extends React.Component{
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <p>Welcome to React!</p>
+      </div>
+    )
+  }
+}
+
+export default Hello;
