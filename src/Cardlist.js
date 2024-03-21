4ca59fd6d81c950bc5eb6f44141f3ea0aa69aec2
Jesse Smart <jsmartdev@protonmail.com>
2024-03-21 18:07:18 -0400

    renamed cardSearch to cardsArray

--- src/Cardlist.js
+++ src/Cardlist.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Card from './Card';
 const Cardlist = ({robots}) => {
-  const cardSearch = robots.map((user, i) => {
+  const cardsArray = robots.map((user, i) => {
     return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />  
   }) 
   return (
     <div>
-      {cardSearch}
+      {cardsArray}
     </div>
   )
 } 
