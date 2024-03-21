a8eb146128f00084deb361e9d811abc093994fbb
Jesse Smart <jsmartdev@protonmail.com>
2024-03-21 19:55:03 -0400

    search handler function

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import Cardlist from './Cardlist';
 import Search from './Search';
 import { robots } from './robots';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Robofriends</h1>
-      <Search />
-      <Cardlist robots={ robots } />
-    </div>
-  )
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>Robofriends</h1>
+        <Search />
+        <Cardlist robots={this.state.robots} />
+      </div>
+    )
+  }
+  
 }
 
 export default App;
